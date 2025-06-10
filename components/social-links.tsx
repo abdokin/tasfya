87d2c1a18d38fe037e44262d2dc8b5f8cf2fc040
abdokin <bashbin.sh@gmail.com>
2025-06-10 14:55:25 +0100

    Remove facebook and instagram links

--- components/social-links.tsx
+++ components/social-links.tsx
@@ -1,17 +1,17 @@
 import Link from "next/link";
 
 const socialLinks = [
-  {
-    name: "Instagram",
-    href: "https://instagram.com",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
-        <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
-        <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
-      </svg>
-    ),
-  },
+  // {
+  //   name: "Instagram",
+  //   href: "https://instagram.com",
+  //   icon: (
+  //     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+  //       <rect x="2" y="2" width="20" height="20" rx="5" ry="5"></rect>
+  //       <path d="M16 11.37A4 4 0 1 1 12.63 8 4 4 0 0 1 16 11.37z"></path>
+  //       <line x1="17.5" y1="6.5" x2="17.51" y2="6.5"></line>
+  //     </svg>
+  //   ),
+  // },
   {
     name: "Youtube",
     href: "https://www.youtube.com/@mohammed-ramzan",
@@ -31,15 +31,15 @@ const socialLinks = [
       </svg>
     ),
   },
-  {
-    name: "Facebook",
-    href: "https://facebook.com",
-    icon: (
-      <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
-        <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
-      </svg>
-    ),
-  }
+  // {
+  //   name: "Facebook",
+  //   href: "https://facebook.com",
+  //   icon: (
+  //     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+  //       <path d="M18 2h-3a5 5 0 0 0-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 0 1 1-1h3z"></path>
+  //     </svg>
+  //   ),
+  // }
 ];
 
 export default function SocialLinks() {
